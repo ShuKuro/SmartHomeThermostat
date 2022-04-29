90d44291edb75421e4f7623d50206c92652e068e
Shuhei Kuroda <shu.0915@gmail.com>
2022-04-29 11:44:37 +0900

    View完成

--- SmartHomeThermostat/ThermometerView.swift
+++ SmartHomeThermostat/ThermometerView.swift
@@ -15,10 +15,30 @@ enum Status: String {
 
 struct ThermometerView: View {
   private let ringSize: CGFloat = 220
-  @State private var ringValue: CGFloat = 0.5
+  private let outerDialSize: CGFloat = 200
+  private let minTemperature: CGFloat = 4
+  private let maxTemperature: CGFloat = 30
+
+  @State private var currentTemperature: CGFloat = 0
   @State private var degrees: CGFloat = 36
 
-  
+  var targetTemperature: CGFloat {
+    return min(max(degrees / 360 * 40, minTemperature), maxTemperature)
+  }
+
+  var ringValue: CGFloat {
+    return currentTemperature / 40
+  }
+
+  var status: Status {
+    if currentTemperature < targetTemperature {
+      return .heating
+    } else if currentTemperature > targetTemperature {
+      return .cooling
+    } else {
+      return .reaching
+    }
+  }
   var body: some View {
     ZStack {
       // MARK: Thermometer Scale
@@ -38,9 +58,29 @@ struct ThermometerView: View {
 
       // MARK: Thermometer Dial
       ThermometerDialView(degrees: degrees)
+        .gesture(
+          DragGesture()
+            .onChanged({ value in
+              let x = min(max(value.location.x, 0), outerDialSize)
+              let y = min(max(value.location.y, 0), outerDialSize)
+
+              let endPoint = CGPoint(x: x, y: y)
+              let centerPoint = CGPoint(x: outerDialSize / 2, y: outerDialSize / 2)
+
+              let angle = calculateAngle(centerPoint: centerPoint, endPoint: endPoint)
+
+              if angle < 36 || angle > 270 { return }
+
+              degrees = angle - angle.remainder(dividingBy: 9)
+            })
+        )
 
       // MARK: Thermometer Summary
-      ThermometerSummaryView(status: .heating, showStatus: true, temperature: 22)
+      ThermometerSummaryView(status: status, showStatus: true, temperature: currentTemperature)
+    }
+    .onAppear {
+      currentTemperature = 22
+      degrees = currentTemperature / 40 * 360
     }
     
   }
@@ -51,5 +91,14 @@ struct ThermometerView_Previews: PreviewProvider {
     ThermometerView()
       .frame(maxWidth: .infinity, maxHeight: .infinity)
       .background(Color("Background"))
+  }
+}
+
+extension ThermometerView {
+  private func calculateAngle(centerPoint: CGPoint, endPoint: CGPoint) -> CGFloat {
+    let radians = atan2(endPoint.x - centerPoint.x, centerPoint.y - endPoint.y)
+    let degrees = 180 + (radians * 180 / .pi)
+
+    return degrees
   }
 }
